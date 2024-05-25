459f46d55a1cb202faf7292069d0ac5120333372
Ellie D <120728802+EllieD33@users.noreply.github.com>
2024-05-25 17:25:00 +0100

    adds profile section

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ import  {MdNightlightRound, MdWbSunny}  from 'react-icons/md'
 const Header = ({ darkMode, setDarkMode }) => {
     return <header className="bg-white dark:bg-gray-950 text-gray-900 dark:text-white">
         <nav className="flex justify-between items-center p-5">
-            <h1 className="text-4xl">Ellen Daly</h1>
+            <h1 className="text-lg">Portfolio</h1>
             <div onClick={() => setDarkMode(!darkMode)}>
                 {
                     darkMode ? <MdWbSunny className="text-2xl cursor-pointer"/> : <MdNightlightRound className="text-2xl cursor-pointer"/>
