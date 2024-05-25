bbe3ab8471cfb97a4b8dfdeb421542a6a11b1296
Ellie D <120728802+EllieD33@users.noreply.github.com>
2024-05-25 14:31:01 +0100

    sets up darkmode

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,16 @@
-import react from "react";
+import React from "react";
+import  {MdNightlightRound, MdWbSunny}  from 'react-icons/md'
 
-const Header = () => {
-    return <div>HEADER</div>
+const Header = ({ darkMode, setDarkMode }) => {
+    return <header className="bg-white dark:bg-gray-950 text-gray-900 dark:text-white">
+        <nav className="flex justify-between items-center p-5">
+            <h1 className="text-4xl">Ellen Daly</h1>
+            <div onClick={() => setDarkMode(!darkMode)}>
+                <MdNightlightRound className="text-2xl cursor-pointer"/>
+                <MdWbSunny className="text-2xl cursor-pointer"/>
+            </div>
+        </nav>
+    </header>
 };
 
 export default Header;
